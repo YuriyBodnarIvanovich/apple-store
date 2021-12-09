e72b9a9137560071d2d0e2938cd1e6b80ab6fc94
urabodnargood <urabodnargood@gmail.com>
2021-12-09 21:07:10 +0200

    add user interface

--- src/redux/mainPage-reducer.js
+++ src/redux/mainPage-reducer.js
@@ -2,7 +2,31 @@ const CHNAGE_TITLE = 'CHNAGE_TITLE';
 
 const intialState = {
     title: 'hello team!',
+
+    user:{
+        idUser: '',//id of user
+        idAuth0:'',// id from Auth0
+        name:'',// name of user
+        email:'',// email user
+        avatar:'',// photo of user
+        cartList:[// корзинка
+           {
+                idCart: '',//id of card 
+                idProduct:'',// id of product
+                name:'',// name of product
+                price:'',// price of product
+                color:'',// color of product
+           }
+
+        ],
+        roles:[
+            {
+                role:'',// role of user  admin/seller/user
+            }
+        ],
+    }
 };
+console.log(JSON.stringify(intialState.user))
 
 const mainPageReducer= (state=intialState, action) =>{
     switch(action.type){
