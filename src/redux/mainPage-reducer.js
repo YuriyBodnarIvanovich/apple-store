6521b0760d03c365f19c2e5a029f0ab399372ce8
maximusyk <maksymusyk@maximusyks-MacBook-Pro.local>
2021-12-09 22:10:57 +0200

    Fix conflicts

--- src/redux/mainPage-reducer.js
+++ src/redux/mainPage-reducer.js
@@ -1,44 +1,44 @@
 const CHNAGE_TITLE = 'CHNAGE_TITLE';
 
 const intialState = {
-  title: 'hello team!',
+    title: 'hello team!',
 
-  user: {
-    idUser: '', //id of user
-    idAuth0: '', // id from Auth0
-    name: '', // name of user
-    email: '', // email user
-    avatar: '', // photo of user
-    cartList: [
-      // корзинка
-      {
-        idCart: '', //id of card
-        idProduct: '', // id of product
-        name: '', // name of product
-        price: '', // price of product
-        color: '', // color of product
-      },
-    ],
-    roles: [
-      {
-        role: '', // role of user  admin/seller/user
-      },
-    ],
-  },
-};
-console.log(JSON.stringify(intialState.user));
+    user:{
+        idUser: '',//id of user
+        idAuth0:'',// id from Auth0
+        name:'',// name of user
+        email:'',// email user
+        avatar:'',// photo of user
+        cartList:[// корзинка
+           {
+                idCart: '',//id of card 
+                idProduct:'',// id of product
+                name:'',// name of product
+                price:'',// price of product
+                color:'',// color of product
+           }
 
-const mainPageReducer = (state = intialState, action) => {
-  switch (action.type) {
-    case CHNAGE_TITLE: {
-      return {
-        ...state,
-        title: action.title,
-      };
+        ],
+        roles:[
+            {
+                role:'',// role of user  admin/seller/user
+            }
+        ],
     }
-    default:
-      return state;
-  }
 };
+console.log(JSON.stringify(intialState.user))
+
+const mainPageReducer= (state=intialState, action) =>{
+    switch(action.type){
+        case CHNAGE_TITLE:{
+            return {
+                ...state,
+                title: action.title
+            }
+        }
+        default:
+            return state;
+    }
+}
 
 export default mainPageReducer;
